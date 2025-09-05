d667801b0e226d4d28164c63978c0219a18f6775
Riley Mullins <130869451+rileymullins@users.noreply.github.com>
2025-09-05 12:09:31 -0500

    Update step_3_generate_peak_by_group_count_matrices.py

--- step_3_generate_peak_by_group_count_matrices.py
+++ step_3_generate_peak_by_group_count_matrices.py
@@ -6,7 +6,7 @@ have peak_id as rows and the insertion count of each group as columns.
 
 --merge_distance (default 250) argument lets you merge any resized peaks that are ≤ N bp apart.
    Internally this is done with pybedtools' BedTool.merge(d=N).
-   disables merging.
+
 
 """
 
